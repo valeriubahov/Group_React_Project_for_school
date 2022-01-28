c141b8e59eeeb5604e51479cfac523bd52525586
Valeriu Bahov <bahovvaleriu@gmail.com>
2022-01-28 10:04:34 -0400

    first commit

---
+++ full-stack-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Description from './Description';
+
+function App() {
+  return (
+    <div className="App">
+      <Description/>
+    </div>
+  );
+}
+
+export default App;
---
+++ full-stack-project/src/Description.js
@@ -0,0 +1,10 @@
+function Description() {
+    return (
+    <div className="Description" > 
+        <p>Image description</p>
+        <p>Location</p>
+        <p>Price</p>
+    </div>);
+}
+
+export default Description;
