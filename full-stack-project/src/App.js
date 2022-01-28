27b06a652d2ea18c6375c191a4a9c9a005549298
Valeriu Bahov <bahovvaleriu@gmail.com>
2022-01-28 10:15:32 -0400

    Commit price test

--- full-stack-project/src/App.js
+++ full-stack-project/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Description from './Description';
+import Price from './Price';
 
 function App() {
   return (
     <div className="App">
-      
+      <Price/>
       <Description/>
     </div>
   );
---
+++ full-stack-project/src/Price.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Price() {
+  return (
+    <div className="Price">
+      <p>To much for me to afford it</p>
+    </div>
+  );
+}
+
+export default Price;
