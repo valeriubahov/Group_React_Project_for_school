f980b232823c441a8b75e7b26bcc3a6ddc71cc71
Mykyta <mykyta.y@hotmail.com>
2022-01-31 09:50:08 -0400

    css

--- full-stack-project/src/Image.js
+++ full-stack-project/src/Image.js
@@ -4,7 +4,7 @@ const ImageComp = function (props)
     const ItemImage = "../honeybadger1.jpeg"; //insert image URL or file name from public folder here
     const ItemIMG = React.createElement("img",{src: ItemImage, className: "itemImage"});
     return(
-        <div id = "itemIMGDiv">
+        <div id = "itemIMGDiv" >
             {ItemIMG}
         </div>
     );
