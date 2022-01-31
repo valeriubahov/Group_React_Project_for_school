dfab13079bfd54b4183d4fca2bb3a2ed4a8f0ce6
Adam Watts <a_watts@outlook.com>
2022-01-31 09:31:48 -0400

    saved honeybader1 image and changed image code to show it

--- full-stack-project/src/Image.js
+++ full-stack-project/src/Image.js
@@ -1,7 +1,7 @@
 import React from "react";
 const ImageComp = function (props)
 {
-    const ItemImage = "../logo192.png"; //insert image URL or file name from public folder here
+    const ItemImage = "../honeybadger1.jpeg"; //insert image URL or file name from public folder here
     const ItemIMG = React.createElement("img",{src: ItemImage, className: "itemImage"});
     return(
         <div id = "itemIMGDiv">
