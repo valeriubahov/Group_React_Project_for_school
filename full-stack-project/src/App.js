e59dac8dcf3ce85eda5e6f06d808f88857b95457
Mykyta <mykyta.y@hotmail.com>
2022-01-31 10:06:16 -0400

    fixed to lower case

--- full-stack-project/src/App.js
+++ full-stack-project/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Description from './components/Description/Description';
 import Price from './components/Price/Price';
-import Button from './components/Buttons/button.js';
+import Button from './components/buttons/button.js';
 import Image from './components/Images/Image';
 import Location from './components/Location/Location';
 
